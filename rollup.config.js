45743dbb77c1885ece5e252c97afad2844130593
outlau <la-bobtheko@hotmail.com>
2021-12-30 16:52:07 +0100

    add NODE_ENV to build step

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 import json from '@rollup/plugin-json';
 
-const production = !process.env.ROLLUP_WATCH;
+const production = process.env.NODE_ENV === 'production';
 
 function serve() {
   let server;
